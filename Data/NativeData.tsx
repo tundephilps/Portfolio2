83f7059907dd73422ca7f8883e9b4ce3846655b6
Tundephilps <tundephilps@gmail.com>
2023-08-26 18:04:36 +0100

    React Native Projects

--- Data/NativeData.tsx
+++ Data/NativeData.tsx
@@ -2,6 +2,27 @@ import { NativeProps } from "@/Types";
 const Nativeprojects: NativeProps[] = [
   {
     id: 1,
+    img: "/News.png",
+    name: "News App",
+    tools: ["HTML", "CSS", "Typescript", "Redux Toolkit", "React Native"],
+
+    link: "https://github.com/tundephilps/ReactNative-NewsApp",
+
+    github: "https://github.com/tundephilps/ReactNative-NewsApp",
+  },
+  {
+    id: 2,
+    img: "/Travel.png",
+    name: "Travel App with Translator ",
+    tools: ["HTML", "CSS", "Javascript", "React Native"],
+
+    link: "https://github.com/tundephilps/React-Native-Travellers-App",
+
+    github: "https://github.com/tundephilps/React-Native-Travellers-App",
+  },
+
+  {
+    id: 3,
     img: "/native.webp",
     name: "Coming Soon ",
     tools: ["HTML", "CSS", "Javascript", "React Native"],
